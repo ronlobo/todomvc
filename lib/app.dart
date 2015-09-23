0186526027f18b7468fd245e6ad322226db880ed
Kasper Peulen <kasperpeulen@gmail.com>
2015-09-23 07:39:43 +0200

    fix indentation

--- lib/app.dart
+++ lib/app.dart
@@ -8,19 +8,19 @@ import 'package:todomvc/components/todo_cmp.dart';
 @View(
     template: '''
     <div class="github-fork-ribbon-wrapper right">
-		  <div class="github-fork-ribbon">
-				<a href="https://github.com/ng2-dart-samples/todomvc">Fork me on GitHub</a>
-		  </div>
-     </div>
+	    <div class="github-fork-ribbon">
+		    <a href="https://github.com/ng2-dart-samples/todomvc">Fork me on GitHub</a>
+	    </div>
+    </div>
     <section class="todoapp">
-	    <router-outlet></router-outlet>
-    </section>
-    <footer class="info">
-	    <p>Double-click to edit a todo</p>
-	    <p>Created by <a href="https://github.com/kasperpeulen">Kasper Peulen</a></p>
-	    <p>Source at <a href="https://github.com/ng2-dart-samples/todomvc">github.</a></p>
-	    <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
-    </footer>
+			<router-outlet></router-outlet>
+		</section>
+		<footer class="info">
+			<p>Double-click to edit a todo</p>
+			<p>Created by <a href="https://github.com/kasperpeulen">Kasper Peulen</a></p>
+			<p>Source at <a href="https://github.com/ng2-dart-samples/todomvc">github.</a></p>
+			<p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
+		</footer>
     ''',
     directives: const [ROUTER_DIRECTIVES, CORE_DIRECTIVES, TodoComponent])
 @RouteConfig(const [const Route(path: '/:filter', component: TodoComponent)])

3e3f052be0a71f9ebde0532f78ca1a1ba777af64
Kasper Peulen <kasperpeulen@gmail.com>
2015-09-23 07:40:59 +0200

    put app view in app.html

--- lib/app.dart
+++ lib/app.dart
@@ -6,22 +6,7 @@ import 'package:todomvc/components/todo_cmp.dart';
 
 @Component(selector: 'app')
 @View(
-    template: '''
-    <div class="github-fork-ribbon-wrapper right">
-	    <div class="github-fork-ribbon">
-		    <a href="https://github.com/ng2-dart-samples/todomvc">Fork me on GitHub</a>
-	    </div>
-    </div>
-    <section class="todoapp">
-			<router-outlet></router-outlet>
-		</section>
-		<footer class="info">
-			<p>Double-click to edit a todo</p>
-			<p>Created by <a href="https://github.com/kasperpeulen">Kasper Peulen</a></p>
-			<p>Source at <a href="https://github.com/ng2-dart-samples/todomvc">github.</a></p>
-			<p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
-		</footer>
-    ''',
+    templateUrl: 'app.html',
     directives: const [ROUTER_DIRECTIVES, CORE_DIRECTIVES, TodoComponent])
 @RouteConfig(const [const Route(path: '/:filter', component: TodoComponent)])
 class App {}

7f882e48e39cbc731c93a769c11ccfab8ea25726
Kasper Peulen <kasperpeulen@gmail.com>
2015-09-23 07:32:46 +0200

    fix indentation

--- lib/app.dart
+++ lib/app.dart
@@ -6,12 +6,12 @@ import 'package:todomvc/components/todo_cmp.dart';
 
 @Component(selector: 'app')
 @View(
-    template: '''
-    <div class="github-fork-ribbon-wrapper right">
-        <div class="github-fork-ribbon">
-            <a href="https://github.com/ng2-dart-samples/todomvc">Fork me on GitHub</a>
-        </div>
-    </div>
+		template: '''
+		<div class="github-fork-ribbon-wrapper right">
+				<div class="github-fork-ribbon">
+						<a href="https://github.com/ng2-dart-samples/todomvc">Fork me on GitHub</a>
+				</div>
+		</div>
 		<section class="todoapp">
 			<router-outlet></router-outlet>
 		</section>
@@ -21,7 +21,7 @@ import 'package:todomvc/components/todo_cmp.dart';
 			<p>Source at <a href="https://github.com/ng2-dart-samples/todomvc">github.</a></p>
 			<p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
 		</footer>
-    ''',
-    directives: const [ROUTER_DIRECTIVES, CORE_DIRECTIVES, TodoComponent])
+		''',
+		directives: const [ROUTER_DIRECTIVES, CORE_DIRECTIVES, TodoComponent])
 @RouteConfig(const [const Route(path: '/:filter', component: TodoComponent)])
 class App {}

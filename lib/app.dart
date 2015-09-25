d8c063e206388a1ffb0869d31cbec356c6dc8db8
Kasper Peulen <kasperpeulen@gmail.com>
2015-09-25 13:07:13 +0200

    remove directives which are not used

--- lib/app.dart
+++ lib/app.dart
@@ -5,8 +5,6 @@ import 'package:angular2/router.dart';
 import 'package:todomvc/components/todo_cmp.dart';
 
 @Component(selector: 'app')
-@View(
-    templateUrl: 'app.html',
-    directives: const [ROUTER_DIRECTIVES, CORE_DIRECTIVES, TodoComponent])
+@View(templateUrl: 'app.html', directives: const [ROUTER_DIRECTIVES])
 @RouteConfig(const [const Route(path: '/:filter', component: TodoComponent)])
 class App {}
